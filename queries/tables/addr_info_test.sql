b36bcba9e6433b886f4fcf38ecaa7b23338143eb
MrKeeplearning <jaeseok1115@gmail.com>
2023-12-03 16:05:04 +0900

    D4-44 test(addr_info_test.sql): addr_info 테이블 테스트 데이터 생성

---
+++ queries/tables/addr_info_test.sql
@@ -0,0 +1,33 @@
+show tables;
+select *
+from addr_info;
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('1111013500', '서울특별시', '종로구', '관철동', '111104100307', '종로12길 15', '03190',
+        '종로코아', '3층');
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('1117013000', '서울특별시', '용산구', '이태원동', '111703102009', '이태원로 145', '04351',
+        '동호프라자', '2층');
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('1117013000', '서울특별시', '중량구', '면목동', '112603005029', '겸재로 240', '02200',
+        '행복오피스텔', '지하 1층');
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('1123010200', '서울특별시', '동대문구', '용두동', '112303105008', '왕산로 122', '02566',
+        '한방천하 용두동 포스빌', '지상 2층');
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('3017010800', '대전광역시', '서구', '괴정동', '301703166004', '갈마로 251', '35294',
+        '한민쇼핑', '한민프라자 2층');
+
+INSERT INTO addr_info (stat_code, city, sigungu, eupmyeondong, street_code, street_name, zip_code,
+                       building, building_details)
+VALUES ('2635010600', '부산광역시', '해운대구', '중동', '263503133039', '좌동순환로 473', '48097',
+        '해운대로데오아울렛', '2층');
