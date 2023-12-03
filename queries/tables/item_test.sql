1245fbc982fb11c1e50cdb97258a17f62b7f30b3
MrKeeplearning <jaeseok1115@gmail.com>
2023-12-03 21:02:31 +0900

    D4-44 test(item_test.sql): item 테이블 테스트 데이터 생성

---
+++ queries/tables/item_test.sql
@@ -0,0 +1,26 @@
+SELECT * FROM item;
+
+INSERT INTO item (description, name, item_type, expire_date, cost_price, grade, category)
+VALUES ('토핑이 쏟아지는 명장면을 포착하세요!',
+        '[3개 묶음] 비요뜨 쵸코링 138g',
+        '냉장', '2024-01-03', 4590, 5, '우유·유제품') ;
+
+INSERT INTO item (description, name, item_type, expire_date, cost_price, grade, category)
+VALUES ('해동 없이 전자레인지로 1분 40초 조리해드세요.',
+        '[배민 이지] 크리스피 핫도그 500g(5개입)',
+        '냉동', '2024-08-30', 7990, 5, '분식·야식');
+
+INSERT INTO item (description, name, item_type, expire_date, cost_price, grade, category)
+VALUES ('소고기를 듬뿍 넣어 묵직하고 진한 국물 맛을 느낄 수 있는 북창동 소고기 순두부 이제 집에서도 만나보세요.',
+        '북창동 소고기 순두부찌개 620g',
+        '냉동', '2024-06-10', 10990, 3, '국·탕·찌개');
+
+INSERT INTO item (description, name, cost_price, grade, category)
+VALUES ('내 피부에 닿는 거니까 깐깐하게 따져보세요',
+        '[2개 묶음] 답이답이다 물티슈 100매(색상랜덤)',
+        2980, 4, '화장지·생리대');
+
+INSERT INTO item (description, name, cost_price, grade, category)
+VALUES ('빨래에 붙는 잡냄새, 훌훌 떠나보내요',
+        '액츠퍼펙트 세탁세제 실내건조(일반) 용기 3.0L',
+        12990, 5, '세제·방향·탈취');
